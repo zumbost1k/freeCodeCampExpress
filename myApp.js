060a8a813052454f4c6f8070f2afdc1a3553b5b2
zumbost1k <matusevich.mi@gmail.com>
2023-02-06 20:04:50 +0300

    first com

--- myApp.js
+++ myApp.js
@@ -1,39 +1,24 @@
 let express = require('express');
 let app = express();
 
+require('dotenv').config();
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+  app.use("/public", express.static(__dirname + "/public"));
+});
 
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") { res.json({ "message": "HELLO JSON" }); }
+  else { res.json({ "message": "Hello json" }); }
+});
 
+app.get('/now', function (req, res, next) {
+  req.time = Date().toString();  // Hypothetical synchronous operation
+  next();
+}, function (req, res) {
+  res.send({ time: req.time });
+});
+app.listen(3000, () => {
+  console.log("my app listening 3000 port")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+});
